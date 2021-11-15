0d44dbaf5793fa02a8e392af5914cb4d241b9c73
Kyle Nickel <1135915+nickelkr@users.noreply.github.com>
2021-11-14 21:55:11 -0500

    Formatting

--- game/games.go
+++ game/games.go
@@ -1,59 +1,59 @@
 package game
 
 import (
-  "encoding/json"
-  "errors"
-  "fmt"
-  "net/http"
-  "strconv"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
 )
 
 type Games struct {
-  games []Game
-  seq int
+	games []Game
+	seq   int
 }
 
 func NewGames() Games {
-  return Games { seq: 0 }
+	return Games{seq: 0}
 }
 
 func (g *Games) AddGame(name string) {
-  g.games = append(g.games, NewGame(name))
+	g.games = append(g.games, NewGame(name))
 }
 
-func (g Games) findGame(id int) (*Game, error){
-  for _, gm := range g.games {
-    if gm.ID == id {
-      return &gm, nil
-    }
-  }
-  return nil, errors.New("Couldn't find game")
+func (g Games) findGame(id int) (*Game, error) {
+	for _, gm := range g.games {
+		if gm.ID == id {
+			return &gm, nil
+		}
+	}
+	return nil, errors.New("Couldn't find game")
 }
 
 func (g Games) Index(w http.ResponseWriter, r *http.Request) {
-  payload, err := json.Marshal(g.games)
-  if err != nil {
-    fmt.Println("Error marshaling games data")
-  }
+	payload, err := json.Marshal(g.games)
+	if err != nil {
+		fmt.Println("Error marshaling games data")
+	}
 
-  fmt.Fprintf(w, string(payload))
+	fmt.Fprintf(w, string(payload))
 }
 
 func (g Games) Show(w http.ResponseWriter, r *http.Request) {
-  id, err := strconv.ParseInt(r.URL.Path[len("/games/"):], 10, 32)
-  if err != nil {
-    fmt.Println("Couldn't parse game ID in show")
-  }
+	id, err := strconv.ParseInt(r.URL.Path[len("/games/"):], 10, 32)
+	if err != nil {
+		fmt.Println("Couldn't parse game ID in show")
+	}
 
-  game, err := g.findGame(int(id))
-  if err != nil {
-    fmt.Println(err.Error())
-  }
+	game, err := g.findGame(int(id))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-  payload, err := json.Marshal(game)
-  if err != nil {
-    fmt.Println("Error marhaling game data")
-  }
+	payload, err := json.Marshal(game)
+	if err != nil {
+		fmt.Println("Error marhaling game data")
+	}
 
-  fmt.Fprintf(w, string(payload))
+	fmt.Fprintf(w, string(payload))
 }
--- main.go
+++ main.go
@@ -7,16 +7,16 @@ import (
 )
 
 func main() {
-  games := game.NewGames()
-  games.AddGame("whisqy")
-  games.AddGame("SD")
+	games := game.NewGames()
+	games.AddGame("whisqy")
+	games.AddGame("SD")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, "Welcome to the home page")
 	})
 
-  mux.HandleFunc("/games", games.Index)
+	mux.HandleFunc("/games", games.Index)
 	mux.HandleFunc("/games/", games.Show)
 	http.ListenAndServe(":4000", mux)
 
