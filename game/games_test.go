2ce609192ed9bebe39a49a419dc893c89f851315
Kyle Nickel <1135915+nickelkr@users.noreply.github.com>
2021-11-14 22:34:05 -0500

    Basic, borked, endpoint test

--- game/games_test.go
+++ game/games_test.go
@@ -1,32 +1,49 @@
 package game
 
 import (
-  "testing"
+	_ "encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func TestFindGame(t *testing.T) {
-  games := NewGames()
-  games.AddGame("test")
+	games := NewGames()
+	games.AddGame("test")
 
-  game, err := games.findGame(1)
-  if err != nil {
-    t.Errorf("Failed to retrieve game: error returned")
-  }
+	game, err := games.findGame(1)
+	if err != nil {
+		t.Errorf("Failed to retrieve game: error returned")
+	}
 
-  if game.Name != "test" {
-    t.Errorf("Failed to retrieve correct game")
-  }
+	if game.Name != "test" {
+		t.Errorf("Failed to retrieve correct game")
+	}
 }
 
 func TestAddGame(t *testing.T) {
-  games := NewGames()
-  games.AddGame("test")
+	games := NewGames()
+	games.AddGame("test")
 
-  if len(games.games) != 1 {
-    t.Errorf("Failed to add game")
-  }
+	if len(games.games) != 1 {
+		t.Errorf("Failed to add game")
+	}
 
-  if game := games.games[0]; game.Name != "test" {
-    t.Errorf("Failed to AddGame with correct data")
-  }
+	if game := games.games[0]; game.Name != "test" {
+		t.Errorf("Failed to AddGame with correct data")
+	}
+}
+
+func TestShow(t *testing.T) {
+	games := NewGames()
+	games.AddGame("test")
+
+	// TODO: This is actually borked. The ID is persistent across tests., track ID for game and set here
+	req := httptest.NewRequest("GET", "/games/1", nil)
+	w := httptest.NewRecorder()
+	games.Show(w, req)
+
+	if status := w.Code; status != http.StatusOK {
+		t.Errorf("Wrong status code: got %v expected %v", status, http.StatusOK)
+	}
 }
