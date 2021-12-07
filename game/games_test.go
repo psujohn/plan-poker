179e2df3d1b465cb268933c71bb56ce5a4c664b2
Kyle Nickel <1135915+nickelkr@users.noreply.github.com>
2021-12-06 20:30:32 -0500

    Merge game into games

--- game/games_test.go
+++ game/games_test.go
@@ -34,6 +34,23 @@ func checkErr(t *testing.T, err error) {
 	}
 }
 
+func TestSave(t *testing.T) {
+	db := setup(t)
+	defer db.Close()
+
+	tmpGame := NewGame("test")
+	id, err := tmpGame.Save(db)
+	checkErr(t, err)
+
+	row := db.QueryRow("SELECT * FROM games WHERE id = ? LIMIT 1", id)
+
+	var game Game
+	row.Scan(&game.ID, &game.Name)
+	if game.Name != "test" {
+		t.Errorf("Failed to store game with correct data")
+	}
+}
+
 func TestFindGame(t *testing.T) {
 	db := setup(t)
 	defer db.Close()
