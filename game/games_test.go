fe7488bcbc5da645e613f78309cc4d0f1b17d4d4
Kyle Nickel <1135915+nickelkr@users.noreply.github.com>
2021-12-08 21:36:59 -0500

    Set up database for testing

--- game/games_test.go
+++ game/games_test.go
@@ -1,11 +1,54 @@
 package game
 
 import (
+  "database/sql"
 	_ "encoding/json"
 	"net/http"
 	"net/http/httptest"
+  "os"
 	"testing"
+
+  _ "github.com/mattn/go-sqlite3"
 )
+
+func checkErr(t *testing.T, err error) {
+  if err != nil {
+    t.Fatal(err)
+  }
+}
+
+func prepareFromFile(db *sql.DB, path string) (*sql.Stmt, error) {
+  data, err := os.ReadFile(path)
+  if err != nil {
+    return nil, err
+  }
+
+  stmt, err := db.Prepare(string(data))
+  if err != nil {
+    return nil, err
+  }
+
+  return stmt, nil
+}
+
+func dbSetup(t *testing.T) *sql.DB {
+  db, err := sql.Open("sqlite3", ":memory:")
+  checkErr(t, err)
+
+  stmt, err := prepareFromFile(db, "../db/create_games.sql")
+  checkErr(t, err)
+
+  _, err = stmt.Exec()
+  checkErr(t, err)
+
+  stmt, err = prepareFromFile(db, "../db/seed_games.sql")
+  checkErr(t, err)
+
+  _, err = stmt.Exec()
+  checkErr(t, err)
+
+  return db
+}
 
 func TestFindGame(t *testing.T) {
 	games := NewGames(nil)
